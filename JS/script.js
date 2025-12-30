0340fc8d2204c9736049369b98532d0b52dff517
anonymous813ke18 <105976740+anonymous813ke18@users.noreply.github.com>
2025-12-30 16:24:41 +0530

    I added a "Work In Progess" message in the input section, just removing that since the work is now done.

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,3 @@ import { setUpEventListeners } from "./eventListeners.js";
 
 addButton();
 setUpEventListeners();
-
-const input = document.querySelector(".input-container");
-
-input.textContent = "Work In Progress...";
